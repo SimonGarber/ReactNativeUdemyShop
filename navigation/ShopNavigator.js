b0b482d39d78029d5df61e8698897e18af0c1d38
Simon Garber <Simon.garber@gmail.com>
2019-11-25 13:39:27 -0500

    fixed missing drawer icon for products by adding navigation options in the navigator

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -3,14 +3,14 @@ import { createAppContainer } from "react-navigation";
 import { createDrawerNavigator } from "react-navigation-drawer";
 import { createStackNavigator } from "react-navigation-stack";
 import { Platform } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
-import colors from "../constants/colors";
 import ProductDetailsScreen from "../screens/shop/ProductDetailsScreen";
 import CartScreen from "../screens/shop/CartScreen";
 import OrdersScreen from "../screens/shop/OrdersScreen";
-import { Ionicons } from "@expo/vector-icons";
 import UserProductsScreen from "../screens/user/UserProductsScreen";
 import EditProductScreen from "../screens/user/EditProductScreen";
+import colors from "../constants/colors";
 
 const defaultNavOptions = {
   headerStyle: {
@@ -31,6 +31,16 @@ const ProductsNavigator = createStackNavigator(
     Cart: CartScreen
   },
   {
+    navigationOptions: {
+      drawerIcon: drawerConfig => (
+        <Ionicons
+          name={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      )
+    },
+
     defaultNavigationOptions: defaultNavOptions
   }
 );
